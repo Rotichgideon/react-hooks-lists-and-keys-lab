10ea76bcfdad72b2ea2dfb80cd73067612cfe0e2
gideon rotich <gideonrotich2001@gmail.com>
2024-04-29 23:59:39 +0300

    done

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,18 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const nav = links.map((link, index) => {
+    return (
+    <a key={index} href={`#${link}`}>{link}</a>
+    );
+  })
+
+  console.log(nav);
+
+  return (<nav>
+    {nav}
+  </nav>
+  );
 }
 
 export default NavBar;
